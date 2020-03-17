25d76698c7e8b6f3daa557ab30fc71f773fc8f38
etrex kuo <et284vu065k3@gmail.com>
2020-03-17 15:05:11 +0800

    change word again

--- src/actions/handle_follow.ts
+++ src/actions/handle_follow.ts
@@ -1,5 +1,5 @@
 import { LineContext } from "bottender";
 
 export default async function HandleFollow(context: LineContext): Promise<void> {
-  context.sendText("Handle Follow");
+  context.sendText("想尬聊就邀我進群組");
 }
